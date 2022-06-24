1dc27aa29f75c5febb85d3808c3f24ec1e98d2d4
Daniel David <danieldavid0930@gmail.com>
2022-06-24 11:46:46 -0400

    Change image manipulation to use complex numbers
    Change the image manipulation module to use complex numbers instead of
    tuple[int, int] for representing coordinates. For this, add two utility
    functions: to_complex and to_int_tuple. They do exactly what they say
    they do. That is, convert between tuple[int, int] and complex.
    
    The motivation behind this change is that complex numbers are easier
    to scale and add to each other than tuples. If operations were
    vectorized on tuples in Python, then this change would be unnecessary.
    However, they are not. One possible alternative would have been to use
    numpy, for which operations are vectorized, but that would be more
    complicated than this solution.

--- src/spot_it/images.py
+++ src/spot_it/images.py
@@ -4,7 +4,7 @@ import random
 
 from PIL import Image, ImageDraw
 
-from .utils import get_random_pos
+from .utils import get_random_pos, to_complex, to_int_tuple
 
 CIRCLE_SCALE = 4
 BACKGROUND = (255, 255, 255, 0)
@@ -45,7 +45,7 @@ def randomize_image(image: Image.Image, size: int) -> Image.Image:
         )
     ) / image_max_dimension
 
-    resized = image.resize((int(image.size[0] * scale), int(image.size[1] * scale)))
+    resized = image.resize(to_int_tuple(to_complex(image.size) * scale))
     rotated = resized.rotate(random.randrange(360), expand=True, fillcolor=BACKGROUND)
     return rotated
 
@@ -58,7 +58,7 @@ def crop_to_square(image: Image.Image) -> Image.Image:
     This is useful because we want to minimize extra space in the spot-it card.
     If this is impossible, return the image cropped to a square.
     """
-    center = (int(image.size[0] / 2), int(image.size[1] / 2))
+    center = to_complex(image.size) / 2
     image_copy = image.copy()
     circle_draw = ImageDraw.Draw(image_copy)
     for radius in range(
@@ -66,8 +66,8 @@ def crop_to_square(image: Image.Image) -> Image.Image:
     ):
         circle_draw.ellipse(
             [
-                (center[0] - radius, center[1] - radius),
-                (center[0] + radius, center[1] + radius),
+                to_int_tuple(center - radius * (1 + 1j)),
+                to_int_tuple(center + radius * (1 + 1j)),
             ],
             BACKGROUND,
             BACKGROUND,
@@ -77,10 +77,8 @@ def crop_to_square(image: Image.Image) -> Image.Image:
             break
     return image.crop(
         (
-            center[0] - radius,
-            center[1] - radius,
-            center[0] + radius,
-            center[1] + radius,
+            *to_int_tuple(center - radius * (1 + 1j)),
+            *to_int_tuple(center + radius * (1 + 1j)),
         )
     )
 
@@ -96,7 +94,7 @@ def spot_it_card(images: list[Image.Image], size: int) -> Image.Image:
         while pos := get_random_pos(
             int(CIRCLE_SCALE / 2 * size),
             int(diameter / 2),
-            (int(card.size[0] / 2), int(card.size[1] / 2)),
+            to_complex(card.size) / 2,
         ):
             if counter >= 10:
                 counter = 0
@@ -104,9 +102,11 @@ def spot_it_card(images: list[Image.Image], size: int) -> Image.Image:
                 reset += 1
             if reset >= 100:
                 return spot_it_card(images, size)
-            cropped = card.crop((*pos, pos[0] + paste.size[0], pos[1] + paste.size[1]))
+            cropped = card.crop(
+                (*to_int_tuple(pos), to_int_tuple(pos + to_complex(paste.size)))
+            )
             counter += 1
             if Image.composite(cropped, cropped, paste).getbbox() is None:
-                card.paste(paste, pos, paste)
+                card.paste(paste, to_int_tuple(pos), paste)
                 break
     return card
--- src/spot_it/utils.py
+++ src/spot_it/utils.py
@@ -12,20 +12,16 @@ S = TypeVar("S")
 
 
 def get_random_pos(
-    within_radius: int, this_radius: int, plane_center: tuple[int, int]
-) -> tuple[int, int]:
+    within_radius: int, this_radius: int, plane_center: complex
+) -> complex:
     """
     Get a random position for the upper left of a circle with radius `this_radius` within a circle
     with radius `within_radius`.
     """
     radius = random.randrange(within_radius - this_radius - 20)
     theta = random.randrange(360)
-    complex_center = cmath.rect(radius, theta)
-    center = (
-        int(complex_center.real) + plane_center[0],
-        int(complex_center.imag) + plane_center[1],
-    )
-    return (center[0] - this_radius, center[1] - this_radius)
+    center = cmath.rect(radius, theta) + plane_center
+    return center - radius * (1 + 1j)
 
 
 def create_mapping(seq1: Iterable[T], seq2: Iterable[S]) -> dict[T, S]:
@@ -42,3 +38,13 @@ def get_images(directory: str) -> list[Image.Image]:
     for file in iglob("*.png", root_dir=directory):
         images.append(Image.open(join(directory, file)))
     return images
+
+
+def to_int_tuple(complex_number: complex) -> tuple[int, int]:
+    """Make the real and imaginary parts of a complex number into a tuple of integers"""
+    return (int(complex_number.real), int(complex_number.imag))
+
+
+def to_complex(ints: tuple[int, int]) -> complex:
+    """Make a tuple of integers into a complex number"""
+    return ints[0] + ints[1] * 1j
