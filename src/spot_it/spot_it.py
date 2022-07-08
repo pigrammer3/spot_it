9bfac165039b6483b5a52c7b7567fd4572a6de90
Daniel David <danieldavid0930@gmail.com>
2022-07-08 14:04:51 -0400

    Add Progress Bar
    Add a progress bar to the deck-making instead of printing when it
    finishes a card.

--- src/spot_it/spot_it.py
+++ src/spot_it/spot_it.py
@@ -1,11 +1,11 @@
 """The main Spot It! generator module."""
-from . import utils
-from . import projective_plane
-from . import images
+from tqdm import tqdm
 
+from . import images, projective_plane, utils
 
 DIRECTORY = "images"
 OUTPUT_DIR = "output"
+
 
 def main():
     """main()"""
@@ -13,9 +13,9 @@ def main():
     order = projective_plane.get_order(len(list_of_images))
     mapping = utils.create_mapping(projective_plane.all_points(order), list_of_images)
     lines = projective_plane.all_lines(order)
-    for num, line in enumerate(lines, 1):
-        print("Starting image", num)
+    for num, line in enumerate(
+        tqdm(lines, desc="Making card", total=len(list_of_images)), start=1
+    ):
         line_images = list(map(mapping.get, line))
         card = images.spot_it_card(line_images, 500)
         card.save(f"{OUTPUT_DIR}/{num}.png")
-        print("Finished image", num)
